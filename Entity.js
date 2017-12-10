8209104579d55f7d517d97b74f64c0f6a7c8a995
Shaun Lusk <shaun.lusk@gmail.com>
2017-12-09 19:54:58 -0600

    Filling in entity.

--- Entity.js
+++ Entity.js
@@ -1,10 +1,12 @@
 var SL = SL || {};
 
-SL.GridMapEntity = function() {
-
+SL.GridMapEntity = function(props) {
+  props = props || {};
+  this._coords = props.coords;
+  this._type = props.type;
 };
 
-SL.GridMapEntity.prototype.setCoords = function() {};
-SL.GridMapEntity.prototype.getCoords = function() {};
-SL.GridMapEntity.prototype.getType = function() {};
-SL.GridMapEntity.prototype.setType = function() {};
+SL.GridMapEntity.prototype.setCoords = function(coords) {this._coords = coords;};
+SL.GridMapEntity.prototype.getCoords = function() {return this._coords;};
+SL.GridMapEntity.prototype.getType = function() {return this._type;};
+SL.GridMapEntity.prototype.setType = function(type) {this._type = type;};
